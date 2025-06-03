a43874b26b5e59e00ef9138bd33e6427a48a785f
Jayden Mautsa <163717439+mautsajayden@users.noreply.github.com>
2025-06-03 05:42:18 -0400

    Add files via upload

---
+++ Python Journey/Section 1/operators_part2_b.py
@@ -0,0 +1,9 @@
+# Replace the values with numbers
+b1 = 5
+b2 = 8
+
+# This line checks if b1 * b2 is greater than b1 + b2
+b3 = (b1 * b2) > (b1 + b2)
+
+# Don't change the line below
+print(f"b3 = {b3}")
